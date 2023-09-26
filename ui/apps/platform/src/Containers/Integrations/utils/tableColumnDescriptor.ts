6c3347ae6bfda7819beff1e26d6f8c7586362be2
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-09-26 20:55:26 +0200

    feat(hackathon): display expiration date in UI

--- ui/apps/platform/src/Containers/Integrations/utils/tableColumnDescriptor.ts
+++ ui/apps/platform/src/Containers/Integrations/utils/tableColumnDescriptor.ts
@@ -89,6 +89,7 @@ const tableColumnDescriptor: Readonly<IntegrationTableColumnDescriptorMap> = {
         apitoken: [
             { accessor: 'name', Header: 'Name' },
             { accessor: 'role', Header: 'Role' },
+            { accessor: 'expiration', Header: 'Expiration' },
         ],
     },
     notifiers: {
