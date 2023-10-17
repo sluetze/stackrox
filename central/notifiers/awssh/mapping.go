d117a06cf1fc3e6066c6bd7e8a8ded7ec4b2c5be
Nakul Chander <1407686+theencee@users.noreply.github.com>
2023-10-17 13:11:24 -0700

    update awssh mapping for new categories

--- central/notifiers/awssh/mapping.go
+++ central/notifiers/awssh/mapping.go
@@ -36,8 +36,8 @@ var (
 		"system modification":      "Effects",
 		"kubernetes events":        "Unusual Behaviors",
 		"docker cis":               "Software and Configuration Checks/Industry and Regulatory Standards",
-		"zero trust":               "TTPs",
-		"supply chain security":    "TTPs",
+		"zero trust":               "Software and Configuration Checks",
+		"supply chain security":    "Software and Configuration Checks",
 	}
 
 	defaultFindingType = "TTPs"
