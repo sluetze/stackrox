097be880970c7bdddd2752334815090dce6ecd70
davdhacs <105243888+davdhacs@users.noreply.github.com>
2023-10-27 10:30:06 -0600

    other ocp*py jobs

---
+++ scripts/ci/jobs/ocp_sensor_integration_tests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run tests/e2e in a OCP cluster
+"""
+import os
+from runners import ClusterTestRunner
+from pre_tests import PreSystemTests
+from ci_tests import SensorIntegrationOCP
+
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+os.environ["ROX_ACTIVE_VULN_MGMT"] = "true"
+os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
+os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
+
+ClusterTestRunner(
+    pre_test=PreSystemTests(run_poll_for_system_test_images=False),
+    # TODO(ROX-17875): Run the regular SensorIntegration() here after the tests are tuned to work on OCP
+    test=SensorIntegrationOCP(),
+).run()
